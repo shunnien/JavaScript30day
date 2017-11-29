9bbfddd8b4fdeed698ff99d7c028bdf22eb19e48
allen_yu <wiamyu@gmail.com>
2017-11-29 23:17:21 +0800

    remove remark

--- day_01/index.js
+++ day_01/index.js
@@ -1,3 +1,9 @@
+/**
+ * 啟動聲音檔案
+ *
+ * @param {any} e
+ * @returns
+ */
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -5,18 +11,18 @@ function playSound(e) {
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-  //   setTimeout(() => {
-  //     key.classList.remove("playing");
-  //   }, 0.07);
-  //console.log(key);
+}
+/**
+ *
+ *
+ * @param {any} e
+ * @returns
+ */
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
 }
 
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return; // skip it
-  //console.log(e.propertyName);
-  //console.log(this);
-  this.classList.remove("playing");
-}
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
