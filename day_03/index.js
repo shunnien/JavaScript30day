3acbe768c23927cf785f3ca3cfa7a0933d364894
allen_yu <wiamyu@gmail.com>
2017-12-13 20:35:05 +0800

    設定樣式

--- day_03/index.js
+++ day_03/index.js
@@ -5,6 +5,7 @@ function handleUpdate() {
 
   // 利用 dataset 取得單位，取不到時給予空值當作預設值
   const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
