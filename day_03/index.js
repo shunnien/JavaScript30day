f56908c2fbdc57a57d65a031bf3be718128ccbd2
allen_yu <wiamyu@gmail.com>
2017-12-13 20:17:23 +0800

    綁定 input 變更事件

--- day_03/index.js
+++ day_03/index.js
@@ -1,2 +1,7 @@
 const inputs = document.querySelectorAll(".controls input");
 
+function handleUpdate() {
+  console.log(this);
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
