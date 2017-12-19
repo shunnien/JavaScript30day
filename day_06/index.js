9297d89d9b98bacc56dda1c7719c8815ec52f2af
allen_yu <wiamyu@gmail.com>
2017-12-19 19:20:26 +0800

    建立 numberWithCommas 將數字每三碼使用逗號區分

--- day_06/index.js
+++ day_06/index.js
@@ -8,20 +8,28 @@ fetch(endpoint)
   .then(blob => blob.json()) // response , blob.json() 為 Promise
   .then(data => cities.push(...data));
 
- // 關鍵字框查詢建議訊息 function 
-function displayMatches(){
-    console.log("hello");
-    // 先將 cities 全部塞到 suggestions 
-    const html = cities.map(obj => {
-        return `<li><span class="name">${obj.city}, ${obj.state}</span><span class="population">${obj.population}</span></li>`;
-      }).join('');
-    const suggestions = document.querySelector('.suggestions');
-    suggestions.innerHTML = html;
+// 將數字每三碼使用逗號區分
+function numberWithCommas(str) {
+  return str.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+// 關鍵字框查詢建議訊息 function
+function displayMatches() {
+  console.log("hello");
+  // 先將 cities 全部塞到 suggestions
+  const html = cities
+    .map(obj => {
+      return `<li><span class="name">${obj.city}, ${
+        obj.state
+      }</span><span class="population">${obj.population}</span></li>`;
+    })
+    .join("");
+  const suggestions = document.querySelector(".suggestions");
+  suggestions.innerHTML = html;
+}
 
-const searchInput = document.querySelector('.search');
+const searchInput = document.querySelector(".search");
 
 // 關鍵字框綁定 change 與 keyup
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
