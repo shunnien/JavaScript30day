8dd8ebe838e9c720d86d292c7efaa42123078fd1
allen_yu <wiamyu@gmail.com>
2017-12-19 18:50:50 +0800

    關鍵字框綁定 change 與 keyup 關鍵字框查詢建議訊息 function

--- day_06/index.js
+++ day_06/index.js
@@ -7,3 +7,15 @@ const cities = [];
 fetch(endpoint)
   .then(blob => blob.json()) // response , blob.json() 為 Promise
   .then(data => cities.push(...data));
+
+ // 關鍵字框查詢建議訊息 function 
+function displayMatches(){
+    console.log("hello");
+}
+
+
+const searchInput = document.querySelector('.search');
+
+// 關鍵字框綁定 change 與 keyup
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
