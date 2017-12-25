b0b336f0bcad820ba4aa418058386b7f4bdfc22a
allen_yu <wiamyu@gmail.com>
2017-12-25 14:50:37 +0800

    增加 **mouseout** 事件，當滑鼠移出畫布時，此次作畫結束

--- day_08/index.js
+++ day_08/index.js
@@ -62,3 +62,4 @@ canvas.addEventListener("mousedown", e => {
 });
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
