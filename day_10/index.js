e03b4ed6c83cb6279ac853d23f799903c23b5414
allen_yu <wiamyu@gmail.com>
2017-12-27 15:02:25 +0800

    最後的判斷式與調整

--- day_10/index.js
+++ day_10/index.js
@@ -2,19 +2,24 @@ const checkboxes = Array.from(
   document.querySelectorAll('.inbox input[type="checkbox"]')
 );
 
+let checkEle = null;
 function handleCheck(e) {
   // e is MouseEvent
   //console.log(e);
   const self = this;
   if (e.shiftKey && e.target.checked) {
-    console.info("this is shift & checked");
+    //console.info("this is shift & checked");
+    let isBetween = false;
     checkboxes.map(ele => {
-      if (self === ele) {
+      if ((self === ele || checkEle === ele) && checkEle && checkEle !== self) {
         //   console.info("slef equal ele");
         //   console.dir(ele);
+        isBetween = !isBetween;
       }
+      if (isBetween) ele.checked = true;
     });
   }
+  checkEle = self.checked ? self : null;
 }
 
 checkboxes.map(ele => ele.addEventListener("click", handleCheck));
