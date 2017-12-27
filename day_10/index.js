88457989a59bf9bee9376b12d9240ac6c8d2ad3f
allen_yu <wiamyu@gmail.com>
2017-12-27 14:33:43 +0800

    針對 handleCheck 撰寫判斷

--- day_10/index.js
+++ day_10/index.js
@@ -1,11 +1,20 @@
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkboxes = Array.from(
+  document.querySelectorAll('.inbox input[type="checkbox"]')
+);
 
 function handleCheck(e) {
-    // e is MouseEvent
-    //console.log(e);
-    if(e.shiftKey && e.target.checked){
-        console.info("this is shift & checked");
-    }
+  // e is MouseEvent
+  //console.log(e);
+  const self = this;
+  if (e.shiftKey && e.target.checked) {
+    console.info("this is shift & checked");
+    checkboxes.map(ele => {
+      if (self === ele) {
+        //   console.info("slef equal ele");
+        //   console.dir(ele);
+      }
+    });
+  }
 }
 
-Array.from(checkboxes).map(ele => ele.addEventListener("click", handleCheck));
+checkboxes.map(ele => ele.addEventListener("click", handleCheck));
