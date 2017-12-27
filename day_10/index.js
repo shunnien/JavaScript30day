c93ae4f5062aefc8d0ed2a0897070a3fdff11b5a
allen_yu <wiamyu@gmail.com>
2017-12-27 16:48:39 +0800

    延伸課題

--- day_10/index.js
+++ day_10/index.js
@@ -22,4 +22,48 @@ function handleCheck(e) {
   checkEle = self.checked ? self : null;
 }
 
-checkboxes.map(ele => ele.addEventListener("click", handleCheck));
+/**
+ * 延伸部分
+ *
+ * @param {any} e
+ */
+function extenCheck(e) {
+  const self = this;
+  if (!e.target.checked) return;
+
+  // 目前的選取元素
+  const nowSelIndex = checkboxes.findIndex(ele => ele === self);
+
+  if (!e.shiftKey) return;
+
+  const nearMinCheckboxIndex = checkboxes.findIndex(
+    (ele, index) =>
+      index !== nowSelIndex && index < nowSelIndex && ele.checked === true
+  );
+  const nearMaxCheckboxIndex = checkboxes.findIndex(
+    (ele, index) =>
+      index !== nowSelIndex && index > nowSelIndex && ele.checked === true
+  );
+
+  if (nearMaxCheckboxIndex !== -1) {
+    setCheckbox(nowSelIndex, nearMaxCheckboxIndex);
+  }
+
+  if (nearMinCheckboxIndex !== -1) {
+    setCheckbox(nearMinCheckboxIndex, nowSelIndex);
+  }
+}
+
+/**
+ * 設定 checkboxes 為選取
+ *
+ * @param {any} initIndex 索引初始
+ * @param {any} aryLength 迴圈長度
+ */
+function setCheckbox(initIndex, aryLength) {
+  for (let index = initIndex; index < aryLength; index++) {
+    checkboxes[index].checked = true;
+  }
+}
+
+checkboxes.map(ele => ele.addEventListener("click", extenCheck));
