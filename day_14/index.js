11f257d3982a077dab3a1b03a4c96fb9cad3234a
allen_yu <wiamyu@gmail.com>
2018-01-02 15:40:24 +0800

    copy of it.

--- day_14/index.js
+++ day_14/index.js
@@ -5,16 +5,19 @@ console.log(age, age2);
 age = 200;
 console.log(age, age2);
 
-let name = 'Wes';
+let name = "Wes";
 let name2 = name;
 console.log(name, name2);
-name = 'wesley';
+name = "wesley";
 console.log(name, name2);
 
 // Let's say we have an array
 const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 
 // and we want to make a copy of it.
+const team = players;
+
+console.log(players, team);
 
 // You might think we can just do something like this:
 
