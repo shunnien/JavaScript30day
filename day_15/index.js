0973ed47360cee983a5345902b743e69a0562ca6
allen_yu <wiamyu@gmail.com>
2018-01-04 13:42:11 +0800

    讓 items 讀取 localStorage 的資料，沒有的話給予空陣列

--- day_15/index.js
+++ day_15/index.js
@@ -1,3 +1,5 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+
+// 讓 items 讀取 localStorage 的資料，沒有的話給予空陣列
+const items = JSON.parse(localStorage.getItem('items')) || [];
