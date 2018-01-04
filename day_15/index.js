3220c96a9a9220963a659f51e9565c2d6048704f
allen_yu <wiamyu@gmail.com>
2018-01-04 14:55:42 +0800

    事件停止冒泡

--- day_15/index.js
+++ day_15/index.js
@@ -2,4 +2,15 @@ const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 
 // 讓 items 讀取 localStorage 的資料，沒有的話給予空陣列
-const items = JSON.parse(localStorage.getItem('items')) || [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+/**
+ * 將小吃資料新增到 items 中
+ * @param {*} e 
+ */
+function addItem(event) {
+  // 事件停止冒泡  
+  e.preventDefault();
+}
+
+addItems.addEventListener('submit', addItem);
