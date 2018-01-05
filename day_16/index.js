5734d79d8a848c160718370d18d1914326e6ac52
allen_yu <wiamyu@gmail.com>
2018-01-05 20:25:13 +0800

    添加 rount 來四捨五入

--- day_16/index.js
+++ day_16/index.js
@@ -20,16 +20,16 @@ function shadow(e) {
 
   // 當滑鼠移動到文字上的時候
   // 利用目前滑鼠在文字上的位置 X 加上該文字容器的 offsetLeft
-  if(e.target !== this)
-  {
+  if (e.target !== this) {
     mouseX += e.target.offsetLeft;
     mouseY += e.target.offsetTop;
-  } 
+  }
 
-  // 移動的位置
-  const xWalk = mouseX/width * walk;
-  const yWalk = mouseY/height * walk;
-  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0, 0, 0, 1)`; 
+  // 移動的位置 
+  // 添加 rount 來四捨五入
+  const xWalk = Math.round(mouseX / width * walk);
+  const yWalk = Math.round(mouseY / height * walk);
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0, 0, 0, 1)`;
 }
 
 function debounce(func, wait = 20, immediate = true) {
