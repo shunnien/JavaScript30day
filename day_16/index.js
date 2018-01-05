7b77d4ccdecf725531db3ce2567b42461e094d47
allen_yu <wiamyu@gmail.com>
2018-01-05 19:12:07 +0800

    加入 debounce

--- day_16/index.js
+++ day_16/index.js
@@ -5,10 +5,26 @@ const text = hero.querySelector("h1");
  * @param {*} e
  */
 function shadow(e) {
-  //console.log(e);
+  console.count(e);
   // equal let mouseX = e.offsetX, mouseY = e.offsetY;
   let { offsetX: mouseX, offsetY: mouseY } = e;
   //console.log([mouseX, mouseY]);
+}
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 hero.addEventListener("mousemove", shadow);
