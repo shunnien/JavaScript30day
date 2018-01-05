d34893619d76fcd0c025774490a09f939636c97e
allen_yu <wiamyu@gmail.com>
2018-01-05 18:35:25 +0800

    紀錄滑鼠移動的 offsetX, offsetY

--- day_16/index.js
+++ day_16/index.js
@@ -1,11 +1,14 @@
-const hero = document.querySelector('.hero');
-
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
 /**
  * 滑鼠移動變動的陰影
- * @param {*} e 
+ * @param {*} e
  */
 function shadow(e) {
-    
+  //console.log(e);
+  // equal let mouseX = e.offsetX, mouseY = e.offsetY;
+  let { offsetX: mouseX, offsetY: mouseY } = e;
+  //console.log([mouseX, mouseY]);
 }
 
-hero.addEventListener('mousemove',shadow);
+hero.addEventListener("mousemove", shadow);
