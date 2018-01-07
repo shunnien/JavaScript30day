a46d0970110ed5c7f772e9e2537c93a7e8e25fc4
allen_yu <wiamyu@gmail.com>
2018-01-07 16:15:59 +0800

    將結果列表到 html 上

--- day_17/index.js
+++ day_17/index.js
@@ -20,4 +20,6 @@ function strip(bandName) {
 
 const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1:-1);
 
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+
 console.log(sortedBands);
