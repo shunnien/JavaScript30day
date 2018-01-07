d2ef2d4a9dbc3e0f6024fd6adc75b6c7bdf9ef1f
allen_yu <wiamyu@gmail.com>
2018-01-07 16:08:59 +0800

    使用 sort 排序

--- day_17/index.js
+++ day_17/index.js
@@ -13,3 +13,13 @@ const bands = [
   "Anywhere But Here",
   "An Old Dog"
 ];
+
+const sortedBands = bands.sort(function(a, b) {
+  if (a > b) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(sortedBands);
