bc3f82149b345e1312e9fba66adaef7a510dae72
allen_yu <wiamyu@gmail.com>
2018-01-07 16:43:50 +0800

    reduce

--- day_18/index.js
+++ day_18/index.js
@@ -1,7 +1,10 @@
 const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
 
-const seconds = timeNodes.map(node => node.dataset.time).map(timeCode => {
-  const [mins, secs] = timeCode.split(":").map(parseFloat);
-  return mins * 60 + secs;
-  console.log(mins, secs);
-});
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return mins * 60 + secs;
+    console.log(mins, secs);
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
