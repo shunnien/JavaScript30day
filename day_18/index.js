3aaf78c99ad0eb5c3e369c3f35f52208d05d5e7f
allen_yu <wiamyu@gmail.com>
2018-01-07 16:41:45 +0800

    建立轉換的資料

--- day_18/index.js
+++ day_18/index.js
@@ -1,1 +1,7 @@
-const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
+
+const seconds = timeNodes.map(node => node.dataset.time).map(timeCode => {
+  const [mins, secs] = timeCode.split(":").map(parseFloat);
+  return mins * 60 + secs;
+  console.log(mins, secs);
+});
