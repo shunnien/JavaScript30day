679f29defb07627e1ecd803ab97064d04bb5c5ff
allen_yu <wiamyu@gmail.com>
2018-01-07 16:14:03 +0800

    convert arrow function

--- day_17/index.js
+++ day_17/index.js
@@ -15,15 +15,9 @@ const bands = [
 ];
 
 function strip(bandName) {
-  return bandName.replace(/^(a |the |an)/i, "").trim();
+  return bandName.replace(/^(a |the |an)/i, '').trim();
 }
 
-const sortedBands = bands.sort(function(a, b) {
-  if (strip(a) > strip(b)) {
-    return 1;
-  } else {
-    return -1;
-  }
-});
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1:-1);
 
 console.log(sortedBands);
