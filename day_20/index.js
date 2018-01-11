c802d1f4d8d6bb713e0b8f02d9fa3d31b0b333b0
allen_yu <wiamyu@gmail.com>
2018-01-11 10:12:55 +0800

    啟用 interimResults 設定 lang

--- day_20/index.js
+++ day_20/index.js
@@ -1,3 +1,6 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = "en-US";
