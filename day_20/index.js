3194598e8e97778c6c56105b71bf7863a71b0177
allen_yu <wiamyu@gmail.com>
2018-01-12 10:50:47 +0800

    輸出辨識文字內容

--- day_20/index.js
+++ day_20/index.js
@@ -1,5 +1,6 @@
 // 將全域環境中的 SpeechRecognition 指定
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 // 建立一個變數recognition來放為語音識別功能
 const recognition = new SpeechRecognition();
@@ -10,8 +11,8 @@ recognition.interimResults = true;
 // 建立元素 p ，等下識別後的文字將塞入
 let p = document.createElement("p");
 
-// 取得 .words 
-const words = document.querySelector('.words');
+// 取得 .words
+const words = document.querySelector(".words");
 // 將 p 塞入 .words DOM
 words.appendChild(p);
 
@@ -19,14 +20,23 @@ words.appendChild(p);
 recognition.start();
 
 // 當識別結束，重複開始識別
-recognition.addEventListener('end', recognition.start);
+recognition.addEventListener("end", recognition.start);
 
 // 識別產生結果
-recognition.addEventListener('result', e => {
+recognition.addEventListener("result", e => {
   // 識別結果為 SpeechRecognitionResultList 取得 transcript 屬性，就是辨識文字
   // console.log(e.results);
   const transcript = Array.from(e.results)
-      .map(result => result[0])
-      .map(result => result.transcript)
-      .join('');
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+
+    // 輸出辨識文字內容
+    p.textContent = transcript;
+
+  // 確認辨識結束，就產生新的 p 元素
+  if (e.results[0].isFinal) {
+    p = document.createElement("p");
+    words.appendChild(p);
+  }
 });
