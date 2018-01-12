3b169bdff515151dce92c8d5fafe181597e9b9d8
allen_yu <wiamyu@gmail.com>
2018-01-12 10:18:01 +0800

    添加註解，改變 lang

--- day_20/index.js
+++ day_20/index.js
@@ -1,6 +1,8 @@
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+// 將全域環境中的 SpeechRecognition 指定
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+// 建立一個變數recognition來放為語音識別功能
 const recognition = new SpeechRecognition();
+
+// 讓語音識別回傳識別後的資訊（預設為false)
 recognition.interimResults = true;
-recognition.lang = "en-US";
