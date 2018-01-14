5b5fcf710e1393a32f23f94e05e7d9ba5b2a7325
allen_yu <wiamyu@gmail.com>
2018-01-14 13:59:36 +0800

    取得感測器資料

--- day_21/script.js
+++ day_21/script.js
@@ -2,3 +2,14 @@
 const arrow = document.querySelector(".arrow");
 // 取得速度 DOM
 const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  data => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  err => {
+    console.error(err);
+  }
+);
