d7e4d769ffa9b1b47556e2fb8626a077b46aaefd
allen_yu <wiamyu@gmail.com>
2018-01-15 18:02:03 +0800

    添加判斷式

--- day_21/script.js
+++ day_21/script.js
@@ -5,9 +5,9 @@ const speed = document.querySelector(".speed-value");
 
 navigator.geolocation.watchPosition(
   data => {
-    console.log(data);
-    speed.textContent = data.coords.speed;
-    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    //console.log(data);
+    if(data.coords.speed) speed.textContent = data.coords.speed;
+    if(data.coords.heading) arrow.style.transform = `rotate(${data.coords.heading}deg)`;
   },
   err => {
     console.error(err);
