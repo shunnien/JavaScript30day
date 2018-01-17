b6d45a620757cd13cc3c4aaecadf21fe65b5c8f2
allen_yu <wiamyu@gmail.com>
2018-01-17 11:12:55 +0800

    功能模擬

--- day_21/script.js
+++ day_21/script.js
@@ -3,13 +3,19 @@ const arrow = document.querySelector(".arrow");
 // 取得速度 DOM
 const speed = document.querySelector(".speed-value");
 
-navigator.geolocation.watchPosition(
-  data => {
-    //console.log(data);
-    if(data.coords.speed) speed.textContent = data.coords.speed;
-    if(data.coords.heading) arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-  },
-  err => {
-    console.error(err);
-  }
-);
+// navigator.geolocation.watchPosition(
+//   data => {
+//     //console.log(data);
+//     if(data.coords.speed) speed.textContent = data.coords.speed;
+//     if(data.coords.heading) arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+//   },
+//   err => {
+//     console.error(err);
+//   }
+// );
+
+function reLocation() {
+  speed.textContent = Math.floor(Math.random() * 100);
+  arrow.style.transform = `rotate(${Math.floor(Math.random() * 361)}deg)`;
+}
+setInterval(reLocation,1000);
