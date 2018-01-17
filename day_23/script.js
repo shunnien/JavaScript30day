afe100f1cc26a171b1a3b5c1f8ea303c9ac47899
allen_yu <wiamyu@gmail.com>
2018-01-17 15:52:06 +0800

    語音內容為畫面文字

--- day_23/script.js
+++ day_23/script.js
@@ -4,3 +4,6 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
+
+// 語音內容為畫面文字
+msg.text = document.querySelector('[name="text"]').value;
