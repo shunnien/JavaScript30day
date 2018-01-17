3a0dd9036b992483af6235d070441fcb7e38dfc8
allen_yu <wiamyu@gmail.com>
2018-01-17 15:59:59 +0800

    填充語音下拉選單

--- day_23/script.js
+++ day_23/script.js
@@ -7,3 +7,18 @@ const stopButton = document.querySelector("#stop");
 
 // 語音內容為畫面文字
 msg.text = document.querySelector('[name="text"]').value;
+
+// 填充下拉選單
+function populateVoices() {
+  // 取得目前支援的所有語音
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes("en"))
+    .map(
+      voice =>
+        `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+    )
+    .join("");
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
