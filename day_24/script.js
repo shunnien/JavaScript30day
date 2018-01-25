854c06b3b83361568c487b37677e092662ec539b
allen_yu <wiamyu@gmail.com>
2018-01-25 14:59:18 +0800

    加入註解

--- day_24/script.js
+++ day_24/script.js
@@ -1,11 +1,13 @@
 const nav = document.querySelector("#main");
 
 // 取得選單 top
-let topOfNav = nav.offsetTop;
+ topOfNav = nav.offsetTop;
 
 // 修正選單
 function fixNav() {
   if (window.scrollY >= topOfNav) {
+
+    // 增加內文 padding 避免內文因為凍結標題列而被遮蔽
     document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add("fixed-nav");
     
