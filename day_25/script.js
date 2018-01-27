93736cd202a4a6aae380917496944cfaa2002045
allen_yu <wiamyu@gmail.com>
2018-01-27 16:32:42 +0800

    將冒泡與捕獲呈現在 html 上

--- day_25/script.js
+++ day_25/script.js
@@ -1,4 +1,8 @@
 const divs = document.querySelectorAll("div");
+//const button = document.querySelector('button');
+const button = document.querySelector('button[name="once"]');
+const capture = document.querySelector('#capture');
+const bubble = document.querySelector('#bubble');
 
 /**
  * 顯示目前觸發的 DOM 
@@ -7,6 +11,9 @@ const divs = document.querySelectorAll("div");
  */
 function logText(e) {
   console.log(this.classList.value);
+  const p = document.createElement("p");
+  p.textContent = this.classList.value;
+  document.body.appendChild(p);
   // 停止冒泡
   //e.stopPropagation(); // stop bubbling!
   //console.log(this); // 顯示 DOM 
@@ -20,6 +27,18 @@ divs.forEach(div => div.addEventListener("click", logText,{
     capture: false,
     once: true
 }));
+
+// 捕獲
+capture.addEventListener('click',() => {
+    divs.forEach(div => div.addEventListener("click", logText,{
+        capture: true
+    }));
+});
+
+// 冒泡
+capture.addEventListener('click',() => {
+    divs.forEach(div => div.addEventListener("click", logText));
+});
 
 // 使用 button 觀察 console 次數
 button.addEventListener('click', () => {
