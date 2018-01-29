22283169a3f79c6b72d7da69a234a74043d692b3
allen_yu <wiamyu@gmail.com>
2018-01-29 17:51:49 +0800

    取得 arrow container

--- day_26/script.js
+++ day_26/script.js
@@ -1,10 +1,13 @@
 const triggers = document.querySelectorAll('.cool > li');
+// arrow container
+const background = document.querySelector('.dropdownBackground');
 
 /**
  * 移動到元素上的觸發事件
  */
 function handleEnter() {
-    
+    this.classList.add('trigger-enter','trigger-enter-active');
+    background.classList.add('open');
 }
 
 /**
