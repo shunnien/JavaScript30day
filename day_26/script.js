a716fefba4f7c976ec0fbe4fd365ee620f449f36
allen_yu <wiamyu@gmail.com>
2018-01-29 17:18:18 +0800

    建立移動到元素與離開的觸發事件

--- day_26/script.js
+++ day_26/script.js
@@ -1,1 +1,20 @@
 const triggers = document.querySelectorAll('.cool > li');
+
+/**
+ * 移動到元素上的觸發事件
+ */
+function handleEnter() {
+    
+}
+
+/**
+ * 離開元素的觸發事件
+ */
+function handleLeave() {
+    
+}
+
+// 移動到元素上的觸發事件
+triggers.forEach(trigger => trigger.addEventListener('mouseenter',handleEnter));
+// 離開元素的觸發事件
+triggers.forEach(trigger => trigger.addEventListener('mouseleave',handleLeave));
