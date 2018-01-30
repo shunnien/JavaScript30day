85519d9f76e83ef807c3c837600f4fca4e80ccd8
allen_yu <wiamyu@gmail.com>
2018-01-30 18:04:12 +0800

    紀錄卷軸左邊位置

--- day_27/script.js
+++ day_27/script.js
@@ -3,12 +3,14 @@ const slider = document.querySelector(".items");
 
 // 紀錄滑鼠點擊的起始位置
 let startX;
+// 紀錄卷軸左邊位置
+let scrollLeft;
 
 /**
  * 移除樣式
  */
 function handleRemoveActive() {
-  slider.classList.add("active");
+  slider.classList.remove("active");
 }
 /**
  * 滑鼠按鍵按下
@@ -30,13 +32,15 @@ function handleMove(e) {
   if (e.buttons !== 1) return;
   // 停止冒泡
   e.stopPropagation();
+  const walk = e.pageX - slider.offsetLeft - startX;
+  slider.scrollLeft = scrollLeft - walk;
 }
 
 // 滑鼠按鍵按下
 slider.addEventListener("mousedown", handleMouseDown);
-// 滑鼠按鍵鬆開
-slider.addEventListener("mouseup", handleRemoveActive);
 // 滑鼠離開元素
 slider.addEventListener("mouseleave", handleRemoveActive);
+// 滑鼠按鍵鬆開
+slider.addEventListener("mouseup", handleRemoveActive);
 // 滑鼠移動
 slider.addEventListener("mousemove", handleMove);
