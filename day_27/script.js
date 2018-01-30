b5b989ee907ac059f8dae36e64d3c4aa4166684f
allen_yu <wiamyu@gmail.com>
2018-01-30 17:17:40 +0800

    滑鼠移動事件

--- day_27/script.js
+++ day_27/script.js
@@ -1,6 +1,17 @@
 // 取得最外層的 div items
 const slider = document.querySelector(".items");
 
+/**
+ * 移動事件
+ * @param {*} e window.event
+ */
+function handleMove(e) {
+}
+
+/**
+ * 滑鼠移動
+ */
+slider.addEventListener("mousemove", handleMove);
 
 /**
  * 滑鼠按鍵按下
@@ -20,5 +31,5 @@ slider.addEventListener("mouseup", () => {
  * 滑鼠離開元素
  */
 slider.addEventListener("mouseleave", () => {
-    slider.classList.remove("active");
-  });
+  slider.classList.remove("active");
+});
