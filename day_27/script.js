de20489f8cd65582689e1bbefe4bc475d618b79b
allen_yu <wiamyu@gmail.com>
2018-01-30 16:49:03 +0800

    增加滑鼠按鍵點擊與否的樣式變換

--- day_27/script.js
+++ day_27/script.js
@@ -1,2 +1,17 @@
 // 取得最外層的 div items
-const slider = document.querySelector('.items');
+const slider = document.querySelector(".items");
+
+
+/**
+ * 滑鼠按鍵按下
+ */
+slider.addEventListener("mousedown", () => {
+  slider.classList.add("active");
+});
+
+/**
+ * 滑鼠按鍵鬆開
+ */
+slider.addEventListener("mouseup", () => {
+  slider.classList.remove("active");
+});
