03bedf908988a2092f90d29a94da51a6ddf142b6
allen_yu <wiamyu@gmail.com>
2018-01-30 17:54:23 +0800

    將 function 抽離

--- day_27/script.js
+++ day_27/script.js
@@ -3,6 +3,24 @@ const slider = document.querySelector(".items");
 
 // 紀錄滑鼠點擊的起始位置
 let startX;
+
+/**
+ * 移除樣式
+ */
+function handleRemoveActive() {
+  slider.classList.add("active");
+}
+/**
+ * 滑鼠按鍵按下
+ * @param {*} e
+ */
+function handleMouseDown(e) {
+  slider.classList.add("active");
+  // 設定起始位置
+  startX = e.pageX - slider.scrollLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
 /**
  * 移動事件
  * @param {*} e window.event
@@ -15,28 +33,11 @@ function handleMove(e) {
   }
 }
 
-/**
- * 滑鼠移動
- */
+// 滑鼠按鍵按下
+slider.addEventListener("mousedown", handleMouseDown);
+// 滑鼠按鍵鬆開
+slider.addEventListener("mouseup", handleRemoveActive);
+// 滑鼠離開元素
+slider.addEventListener("mouseleave", handleRemoveActive);
+// 滑鼠移動
 slider.addEventListener("mousemove", handleMove);
-
-/**
- * 滑鼠按鍵按下
- */
-slider.addEventListener("mousedown", () => {
-  slider.classList.add("active");
-});
-
-/**
- * 滑鼠按鍵鬆開
- */
-slider.addEventListener("mouseup", () => {
-  slider.classList.remove("active");
-});
-
-/**
- * 滑鼠離開元素
- */
-slider.addEventListener("mouseleave", () => {
-  slider.classList.remove("active");
-});
