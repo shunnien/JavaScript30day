3914760661c5b4eb1e5ba98e1c136dcb57189d95
allen_yu <wiamyu@gmail.com>
2018-02-01 11:37:51 +0800

    速度條上滑鼠移動

--- day_28/script.js
+++ day_28/script.js
@@ -2,3 +2,13 @@
 const speed = document.querySelector(".speed");
 // 取得 speed bar div
 const bar = speed.querySelector(".speed-bar");
+
+/**
+ * 速度條上滑鼠移動
+ * @param {*} e window.event
+ */
+function handleMove(e) {
+    
+}
+
+speed.addEventListener('mousemove', handleMove);
