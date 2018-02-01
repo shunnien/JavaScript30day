921eb974b5830eb0503e11d6e9f0905f6bc8f7ce
allen_yu <wiamyu@gmail.com>
2018-02-01 16:12:36 +0800

    倒數計時的顯示元素 結束時間的顯示元素

--- day_29/script.js
+++ day_29/script.js
@@ -1,2 +1,7 @@
-// 取得所有 buttons 
-const buttons = document.querySelectorAll('[data-time]');
+// 取得所有 buttons
+const buttons = document.querySelectorAll("[data-time]");
+
+// 倒數計時的顯示元素
+const timerDisplay = document.querySelector(".display__time-left");
+// 結束時間的顯示元素
+const endTime = document.querySelector(".display__end-time");
