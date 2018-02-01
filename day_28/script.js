306bfed841dcaee20ca62b09fb929aca3c10e0a9
allen_yu <wiamyu@gmail.com>
2018-02-01 13:29:50 +0800

    取得滑鼠在速度條上的位置

--- day_28/script.js
+++ day_28/script.js
@@ -8,7 +8,12 @@ const bar = speed.querySelector(".speed-bar");
  * @param {*} e window.event
  */
 function handleMove(e) {
+    // 取得滑鼠在速度條上的位置
+    const mousePoint = e.pageY - this.offsetTop;
+    // 計算移動的速度條百分比
+    const mousePercent = mousePoint / this.offsetHeight;
     
+
 }
 
 speed.addEventListener('mousemove', handleMove);
