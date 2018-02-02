23bb423e26ca7fb25bd5517b4f2a63e6f9852159
allen_yu <wiamyu@gmail.com>
2018-02-02 22:53:17 +0800

    計算幾分鐘 計算剩餘秒數

--- day_29/script.js
+++ day_29/script.js
@@ -8,8 +8,11 @@ const endTime = document.querySelector(".display__end-time");
 
 /**
  * 顯示倒數計時
- * @param {*} seconds 秒數 
+ * @param {*} seconds 秒數
  */
 function displayTimeLeft(seconds) {
-    
+  // 計算幾分鐘
+  const minutes = Math.floor(seconds / 60);
+  // 計算剩餘秒數
+  const remainderSeconds = seconds % 60;
 }
