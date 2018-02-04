e847fde70da25e58899dfb6bd0fa0b1db5581997
allen_yu <wiamyu@gmail.com>
2018-02-04 18:12:00 +0800

    地鼠出現的時間

--- day_30/script.js
+++ day_30/script.js
@@ -1,3 +1,12 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+
+/**
+ * 地鼠出現的時間
+ * @param {*} min
+ * @param {*} max
+ */
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
