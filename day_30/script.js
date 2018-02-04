348db5bc6be018406188911bde84a8d7265a6dea
allen_yu <wiamyu@gmail.com>
2018-02-04 18:15:14 +0800

    隨機出現地鼠的地洞

--- day_30/script.js
+++ day_30/script.js
@@ -10,3 +10,13 @@ const moles = document.querySelectorAll(".mole");
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
+
+/**
+ * 隨機出現地鼠的地洞
+ * @param {*} holes 
+ */
+function randomHole(holes) {
+  // holes 是陣列，由 0 開始，所以利用 random 直接設定 random 範圍
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+}
